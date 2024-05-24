d31ec1d606b7bb4043c27bd211e2cfbf73f78c1b
Diana Martseniuk <diana@rectavia.studio>
2024-05-24 16:44:39 +0200

    add names of the skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,14 +6,19 @@ export default function Skills() {
   return (
     <section id="skills">
       <div className="flex bg-light-grey justify-center py-10 h-[90vh]">
-        <div className="w-3/4 flex flex-col gap-10 content-center text-blue">
+        <div className="w-3/4 flex flex-col gap-10 justify-center text-blue">
           <Title title="Skill Highlights" color="blue" />
           <h1 className="font-hind-madurai font-semibold text-[48px] leading-tight self-center">
             Confident in
           </h1>
           <div className="flex justify-center gap-14">
             {data.skills.confident.map((skill) => (
-              <Skill skill={skill} />
+              <div className="flex flex-col items-center gap-2">
+                <Skill skill={skill} />
+                <p className="font-hind-madurai text-[18px] text-light-blue">
+                  {skill}
+                </p>
+              </div>
             ))}
           </div>
           <h1 className="font-hind-madurai font-semibold text-[48px] leading-tight self-center">
@@ -21,7 +26,12 @@ export default function Skills() {
           </h1>
           <div className="flex justify-center gap-14">
             {data.skills.familiar.map((skill) => (
-              <Skill skill={skill} />
+              <div className="flex flex-col items-center gap-2">
+                <Skill skill={skill} />
+                <p className="font-hind-madurai text-[18px] text-light-blue">
+                  {skill}
+                </p>
+              </div>
             ))}
           </div>
         </div>
